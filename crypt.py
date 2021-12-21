38d659ddd98616c07193faec394b09001478ff63
Daniel Acero Sagredo <96375209+danielaceros@users.noreply.github.com>
2021-12-21 17:00:37 +0100

    Add files via upload

---
+++ crypt.py
@@ -0,0 +1,34 @@
+from cryptography.fernet import Fernet
+from os import remove
+from time import sleep
+from tqdm import tqdm
+
+
+def maincrypt():
+    def gen():
+        f = Fernet.generate_key()
+        with open("key.key", "wb") as filekey:
+            filekey.write(f)
+            filekey.close()
+
+    def chrg():
+        return open("key.key", "rb").read()
+
+    def crypt():
+        path = input("> ")
+        fe = Fernet(chrg())
+        with open(path, "rb") as fileread:
+            rd = fileread.read()
+            fileread.close()
+        for i in tqdm(range(1000)):
+            sleep(0.01)
+        print("File CRYPTED")
+        with open(path, "wb") as filewrite:
+            crypt = fe.encrypt(rd)
+            filewrite.write(crypt)
+            filewrite.close()
+
+    gen()
+    chrg()
+    crypt()
+maincrypt()
---
+++ decrypt.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+from os import remove
+from time import sleep
+from tqdm import tqdm
+
+def maindecrypt():
+    def chrg():
+        return open("key.key", "rb").read()
+
+    def decrypt():
+        path = input("> ")
+        fe = Fernet(chrg())
+        with open(path, "rb") as fileread:
+            rd = fileread.read()
+            fileread.close()
+        for i in tqdm(range(1000)):
+            sleep(0.01)
+        print("File DECRYPTED")
+        with open(path, "wb") as filewrite:
+            decrypt = fe.decrypt(rd)
+            filewrite.write(decrypt)
+            filewrite.close()
+
+    chrg()
+    decrypt()
+maindecrypt()
